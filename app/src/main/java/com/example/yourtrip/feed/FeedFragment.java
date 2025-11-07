e53ca9eb6762bea0254b9a7b9f5c5ff46c6a2d52
agunggung22 <jbjokagd@gmail.com>
2025-11-07 18:40:31 +0900

    공통 버튼, 입력창과 공통 색상 작업 완료

--- app/src/main/java/com/example/yourtrip/feed/FeedFragment.java
+++ app/src/main/java/com/example/yourtrip/feed/FeedFragment.java
@@ -1,13 +1,18 @@
 package com.example.yourtrip.feed;
 
+import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.yourtrip.R;
@@ -22,6 +27,7 @@ public class FeedFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_feed, container, false);
 
         Button btnNext = view.findViewById(R.id.btnNext);
+        EditText editDynamic = view.findViewById(R.id.editDynamic);
 
         btnNext.setEnabled(false); // 기본 비활성
 
@@ -29,6 +35,10 @@ public class FeedFragment extends Fragment {
         new android.os.Handler().postDelayed(() -> {
             btnNext.setEnabled(true);
         }, 3000);
+
+
+
+
 
         return view;
     }
