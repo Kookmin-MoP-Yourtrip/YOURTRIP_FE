fd4c7bb7fa44e0b6ad90fb0392e205b6c8e70cfb
dani0910 <dlekdms0910@naver.com>
2025-11-09 03:28:58 +0900

    스플래시 화면 구현 : 그라데이션 배경, 앱 로고 추가

--- app/src/main/java/com/example/yourtrip/LauncherActivity.java
+++ app/src/main/java/com/example/yourtrip/LauncherActivity.java
@@ -12,22 +12,22 @@ public class LauncherActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // ✅ 시스템 스플래시 적용
+        //  시스템 스플래시 적용
         SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
 
-        // ✅ 로그인 상태 확인 (SharedPreferences 등)
+        //  로그인 상태 확인 (SharedPreferences 등)
         boolean isLoggedIn = getSharedPreferences("UserPrefs", MODE_PRIVATE)
                 .getBoolean("isLoggedIn", false);
 
-        // ✅ 분기 이동
+        //  분기 이동
         if (isLoggedIn) {
             startActivity(new Intent(this, MainActivity.class));
         } else {
             startActivity(new Intent(this, LoginActivity.class));
         }
 
-        // ✅ LauncherActivity 종료 (뒤로가기시 안돌아오게)
+        //  LauncherActivity 종료 (뒤로가기시 안돌아오게)
         finish();
     }
 }
