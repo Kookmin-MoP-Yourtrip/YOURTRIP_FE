44bb8bf4f075028288eb3b8fb66d24b67a5d160f
dani0910 <dlekdms0910@naver.com>
2025-11-09 07:56:03 +0900

    로그인 페이지 UI 작업 및 버튼 Intent 연동 완료
    - 로그인 UI 작업 완료.
    - 로그인 버튼의 Intent 연동 완료.
    
    버튼 배경색 문제 해결
    
    - 지정하지도 않은 배경색이 적용되어 변경되지 않던 문제 해결.
    - 	hemes.xml 파일에서 스타일 상속 수정:
      - 기존: Theme.Material3.DayNight.NoActionBar → 변경: Theme.AppCompat.Light.
      - 변경 후 버튼 배경색 정상 적용됨.
    
    상단바 문제 해결 필요
    
    - 버튼 배경색을 수정한 후 로그인 화면에서 상단바가 나타나는 문제 발생.
    - 상단바 코드 파일을 못 찾아서, 상단바가 메인 화면에서만 고정되도록 수정 필요.

--- app/src/main/java/com/example/yourtrip/auth/LoginActivity.java
+++ app/src/main/java/com/example/yourtrip/auth/LoginActivity.java
@@ -1,15 +1,66 @@
+//package com.example.yourtrip.auth;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.view.View;
+//import android.widget.Button;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import com.example.yourtrip.MainActivity;
+//import com.example.yourtrip.R;
+//
+//
+//public class LoginActivity extends AppCompatActivity {
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_login);
+//    }
+//}
+
 package com.example.yourtrip.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
-//R 인식 안돼서 수동으로 넣어줌
+
+import com.example.yourtrip.MainActivity;
 import com.example.yourtrip.R;
 
-
 public class LoginActivity extends AppCompatActivity {
+
+    private Button btnSignUp, btnSkipLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_login); // 로그인 화면 XML
+
+        btnSignUp = findViewById(R.id.btnSignUp);  // 회원가입 버튼
+        btnSkipLogin = findViewById(R.id.tvSkipLogin);  // 로그인 없이 둘러보기 버튼
+
+        // 회원가입 버튼 클릭 시
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // SignUpActivity로 이동
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 로그인 없이 둘러보기 버튼 클릭 시
+        btnSkipLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // MainActivity로 이동
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
