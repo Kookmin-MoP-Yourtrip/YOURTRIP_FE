bb099da20e070cbc2213357e1652742bf1577638
agunggung22 <jbjokagd@gmail.com>
2025-11-13 13:50:14 +0900

    피드카드 레이아웃 만듬 & 피드 리스트 페이지 틀 잡음

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     //스플래시 스크린
     implementation("androidx.core:core-splashscreen:1.0.1")
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
