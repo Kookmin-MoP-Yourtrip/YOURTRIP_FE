8fa43f3be9425eacc2ecc022e1fd89f59a5d66cb
agunggung22 <jbjokagd@gmail.com>
2025-11-18 16:44:41 +0900

    더미 이미지 추가

--- app/src/main/java/com/example/yourtrip/home/HomeFragment.java
+++ app/src/main/java/com/example/yourtrip/home/HomeFragment.java
@@ -12,7 +12,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.yourtrip.R;
-import com.example.yourtrip.adapter.PopularCourseAdapter;
 import com.example.yourtrip.model.HomePopularCourseItem;
 
 import java.util.ArrayList;
@@ -71,18 +70,22 @@ public class HomeFragment extends Fragment {
         dummy.add(new HomePopularCourseItem(
                 "대전 맛도리 빵집 투어",
                 "대전 유성구, 중구",
-                R.drawable.ic_edit
+                R.drawable.dummy1,
+                List.of("자차", "쇼핑")
         ));
         dummy.add(new HomePopularCourseItem(
-                "대전 맛도리 빵집 투어",
-                "대전 유성구, 중구",
-                R.drawable.ic_edit
+                "춘천 감성 카페 투어",
+                "강원 춘천시",
+                R.drawable.dummy3,
+                List.of("감성", "하루")
         ));
 
         dummy.add(new HomePopularCourseItem(
                 "성수 카페 투어",
                 "서울 성동구",
-                R.drawable.ic_go
+                R.drawable.dummy2,
+                List.of("혼자", "쇼핑", "프리미엄")
+
         ));
 
         // Adapter에 데이터 전달
--- app/src/main/java/com/example/yourtrip/home/PopularCourseAdapter.java
+++ app/src/main/java/com/example/yourtrip/home/PopularCourseAdapter.java
@@ -1,4 +1,4 @@
-package com.example.yourtrip.adapter;
+package com.example.yourtrip.home;
 
 import android.content.Context;
 import android.view.LayoutInflater;
