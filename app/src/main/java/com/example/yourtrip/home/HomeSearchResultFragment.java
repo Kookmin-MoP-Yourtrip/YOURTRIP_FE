f6aa64190715eafca3ed417523437feb93968e61
agunggung22 <jbjokagd@gmail.com>
2025-11-20 14:15:37 +0900

    검색 결과에서 검색창 누르면 다시 검색화면으로 이동

--- app/src/main/java/com/example/yourtrip/home/HomeSearchResultFragment.java
+++ app/src/main/java/com/example/yourtrip/home/HomeSearchResultFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -18,6 +19,30 @@ public class HomeSearchResultFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home_search_result, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home_search_result, container, false);
+
+        setupSearchBarClick(view);
+
+        return view;
+    }
+
+    private void setupSearchBarClick(View view) {
+        EditText etSearch = view.findViewById(R.id.tvSearch);
+
+        // 🔹 HomeFragment와 동일하게 클릭 전용으로 설정
+        etSearch.setFocusable(false);
+        etSearch.setClickable(true);
+
+        etSearch.setOnClickListener(v -> {
+            Fragment fragment = new HomeSearchFragment();
+
+            requireActivity()
+                    .getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragmentContainer, fragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
     }
 }
