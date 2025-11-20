374498536657b0d5c9215b80136a785afd28b6a8
dani0910 <dlekdms0910@naver.com>
2025-11-20 13:26:55 +0900

    시연용 임시 코드

--- app/src/main/java/com/example/yourtrip/model/MyCourseListItemResponse.java
+++ app/src/main/java/com/example/yourtrip/model/MyCourseListItemResponse.java
@@ -1,12 +1,15 @@
 package com.example.yourtrip.model;
+import java.io.Serializable;
 
-public class MyCourseListItemResponse {
+
+public class MyCourseListItemResponse implements Serializable {
     private String title;
     private String location;
     private String startDate;
     private String endDate;
     private int memberCount;
 
+    // 생성자
     public MyCourseListItemResponse(String title, String location, String startDate, String endDate, int memberCount) {
         this.title = title;
         this.location = location;
@@ -15,9 +18,87 @@ public class MyCourseListItemResponse {
         this.memberCount = memberCount;
     }
 
-    public String getTitle() { return title; }
-    public String getLocation() { return location; }
-    public String getStartDate() { return startDate; }
-    public String getEndDate() { return endDate; }
-    public int getMemberCount() { return memberCount; }
+    // getter, setter 메서드
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+
+    public int getMemberCount() {
+        return memberCount;
+    }
+
+    public void setMemberCount(int memberCount) {
+        this.memberCount = memberCount;
+    }
 }
+
+
+//package com.example.yourtrip.model;
+//
+//public class MyCourseListItemResponse {
+//    private String title;
+//    private String location;
+//    private String startDate;
+//    private String endDate;
+//    private int memberCount;
+//
+//    public MyCourseListItemResponse(String title, String location, String startDate, String endDate, int memberCount) {
+//        this.title = title;
+//        this.location = location;
+//        this.startDate = startDate;
+//        this.endDate = endDate;
+//        this.memberCount = memberCount;
+//    }
+//
+//    public String getTitle() { return title; }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//    public String getLocation() { return location; }
+//
+//    public void setLocation(String location) {
+//        this.location = location;
+//    }
+//    public String getStartDate() { return startDate; }
+//    public void setStartDate(String startDate) {
+//        this.startDate = startDate;
+//    }
+//    public String getEndDate() { return endDate; }
+//    public void setEndDate(String endDate) {
+//        this.endDate = endDate;
+//    }
+//    public int getMemberCount() { return memberCount; }
+//    public void setMemberCount(int memberCount) {
+//        this.memberCount = memberCount;
+//    }
+//}
+//}
