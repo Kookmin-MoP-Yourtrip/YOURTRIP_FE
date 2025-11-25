6b8564b743f46f2d8a53c7bacea29c9f52142e40
taehwan <th2194@naver.com>
2025-11-25 17:36:07 +0900

    refactor: naver api url

--- app/src/main/java/com/example/yourtrip/network/NaverGeocodeAPI.java
+++ app/src/main/java/com/example/yourtrip/network/NaverGeocodeAPI.java
@@ -2,15 +2,16 @@ package com.example.yourtrip.network;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.Query;
 
 public interface NaverGeocodeAPI {
 
     // 네이버 Geocoding API를 사용하여 주소를 위도와 경도로 변환
-    @GET("map/geocode")
+    @GET("map-geocode/v2/geocode")
     Call<NaverGeocodeResponse> getCoordinates(
-            @Query("clientId") String clientId,
-            @Query("clientSecret") String clientSecret,
+            @Header("X-NCP-APIGW-API-KEY-ID") String clientId,
+            @Header("X-NCP-APIGW-API-KEY") String clientSecret,
             @Query("query") String query
     );
 }
--- app/src/main/java/com/example/yourtrip/network/RetrofitClient.java
+++ app/src/main/java/com/example/yourtrip/network/RetrofitClient.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     private static Retrofit retrofit;
-    private static final String BASE_URL = "https://naveropenapi.apis.naver.com/"; // Geocoding API 주소
+    private static final String BASE_URL = "https://yourtrip.site/";
 
     // 기존 getInstance() 메서드는 유지
     public static Retrofit getInstance() {
